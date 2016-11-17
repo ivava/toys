7a0dfa459f75cb07f207a70b5c99bde4d8b5d6af
miheeva <miheeva.aa@yandex.ru>
2016-11-17 07:38:55 +0300

    0.5

--- assets/js/screen_gallery.js
+++ assets/js/screen_gallery.js
@@ -20,22 +20,33 @@ $(document).ready(function() {
         var carouselControls = $('.carousel_controls');
         var next = carouselControls.find('.next');
         var prev = carouselControls.find('.prev');
-        var count;
-        next.click(function() {
+        var count = 0;
+        next.click(function(event) {
            slides.removeClass('active');
-           if (count >= slides.length) {
-               count = 0;
-           }
-           slidesCount[count].addClass('active');
-            count++;
-        });
-            prev.click(function() {
-                slides.removeClass('active');
-                if (count == 0) {
-                    count = slides.length;
-                    slidesCount[count].addClass('active');
+            event.preventDefault();
+            if (count != 0) {
+                if (count == slides.length) {
+                    count = 0;
+                    $(slidesCount[count]).addClass('active');
+                    count++;
                 } else {
-                    slidesCount[count - 1].addClass('active');
+                    $(slidesCount[count]).addClass('active');
+                    count++;
+                }
+            } else {
+                $(slidesCount[count + 1]).addClass('active');
+                count++;
+            }
+        });
+            prev.click(function(event) {
+                event.preventDefault();
+                slides.removeClass('active');
+                if (count < 0) {
+                    count = slides.length - 1;
+                    console.log(count);
+                    $(slidesCount[count]).addClass('active');
+                } else {
+                    $(slidesCount[count]).addClass('active');
                 }
                 count--;
             })
--- index.php
+++ index.php
@@ -117,11 +117,38 @@ include ("header.php");
     </div>
     <div class="screen_carousel clearfix">
         <div class="row">
+            <div class="col-md-12">
+                <ul class="nav nav-tabs tab_center">
+                    <li class="active"><a href="#screen_first" data-toggle="tab">Body chat</a></li>
+                    <li><a href="#screen_second" data-toggle="tab">Wearables</a></li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="tab-content">
+                <div id="screen_first" class="active tab-pane fade in">
             <div class="col-md-6">
-               <div class="carousel_controls">
-                   <a href="#smartSlide" class="prev"></a>
-                   <a href="#smartSlide" class="next"></a>
-               </div>
+                <div class="carousel_about">
+                    <h2 class="larg margin_heading">Main features</h2>
+                    <ul class="check_marker">
+                        <li>Local Control</li>
+                        <li>Long Distance Control</li>
+                        <li>In App Messaging</li>
+                        <li>Video Chat</li>
+                        <li>Sync to Music</li>
+                        <li>Long Distance Sex Interaction </li>
+                    </ul>
+                    <h2>
+                        Работает с:
+                    </h2>
+                    <ul class="device_ul">
+                        <li><i class="fa fa-apple"></i>iPhone 4S/5/5C/5S/6/6+/6s/6s/7+/SE and later
+                            iPad 3/4/Air and later, iPad Mini 1/2 and later</li>
+                        <li><i class="fa fa-android"></i>Android 4.0 and later with Bluetooth enabled</li>
+                        <li><i class="fa fa-desktop"></i>PC/MAC</li>
+                    </ul>
+                </div>
             </div>
             <div class="col-md-6">
                 <div class="screen_slides">
@@ -139,8 +166,176 @@ include ("header.php");
                         </ul>
                     </div>
                 </div>
+                <div class="carousel_controls clearfix">
+                    <a href="#smartSlide" class="prev">
+                        <i class="fa fa-angle-left"></i>
+                    </a>
+                    <a href="#smartSlide" class="next">
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
             </div>
+                </div>
+                <div id="screen_second" class="tab-pane fade in">
+                    <div class="col-md-6">
+                        <div class="carousel_about">
+                            <h2 class="larg margin_heading">Main features</h2>
+                            <ul class="check_marker">
+                                <li>Local Control</li>
+                                <li>Long Distance Control</li>
+                                <li>In App Messaging</li>
+                                <li>Video Chat</li>
+                                <li>Sync to Music</li>
+                                <li>Long Distance Sex Interaction </li>
+                            </ul>
+                            <h2>
+                                Работает с:
+                            </h2>
+                            <ul class="device_ul">
+                                <li><i class="fa fa-apple"></i>iPhone 4S/5/5C/5S/6/6+/6s/6s/7+/SE and later
+                                    iPad 3/4/Air and later, iPad Mini 1/2 and later</li>
+                                <li><i class="fa fa-android"></i>Android 4.0 and later with Bluetooth enabled</li>
+                                <li><i class="fa fa-desktop"></i>PC/MAC</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="screen_slides">
+                            <div class="slides">
+                                <ul class="slides_container" id="smartSlide">
+                                    <li class="active">
+                                        <img src="assets/img/screen_nora_1_lg.jpg" alt="nora" />
+                                    </li>
+                                    <li>
+                                        <img src="assets/img/screen_nora_2_lg.jpg" alt="nora" />
+                                    </li>
+                                    <li>
+                                        <img src="assets/img/screen_nora_3_lg.jpg" alt="nora" />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="carousel_controls clearfix">
+                            <a href="#smartSlide" class="prev">
+                                <i class="fa fa-angle-left"></i>
+                            </a>
+                            <a href="#smartSlide" class="next">
+                                <i class="fa fa-angle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
+        </div>
+        </div>
+    </div>
+    <div class="big_separate">
+        <h1 class="larg white_text">
+            Take Control. Anytime. Anywhere.
+        </h1>
+    </div>
+    <div class="advantage_instuction">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 content">
+                    <h2 class="larg text_color_base text-center">Как проходит типичная сессия секса на расстоянии?</h2>
+                    <div class="row content_text">
+                <div class="col-md-6">
+                    <div class="wrapper">
+                <p class="larg medium_font">Каждая игрушка имеет модули Bluetooth 2,0 и 4,0 LE.
+                    Если же ваш телефон или компьютер не имеет Bluetooth, не волнуйтесь!
+                    В комплекте с вашей игрушкой, вы получите бесплатный Bluetooth-коннектор,
+                    который позволит вам подключиться к любому компьютеру. </p>
+                        </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="wrapper">
+                 <p class="larg medium_font">Мы предлагаем вам полностью
+                     функциональное программное обеспечение с
+                     поддержкой видеочата, но вы также можете
+                     использовать другой видеочат и пользоваться
+                     нашей программой лишь для соединения игрушек.</p>
+                        </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="wrapper">
+                    <p class="larg medium_font">Начните поддразнивать друг друга в видеочате,
+                        как вы это обычно делаете. Когда вы будете готовы сделать
+                        вашу встречу немного более «интерактивной» (заняться сексом на расстоянии),
+                        начните использовать свои игрушки. Наши игрушки ответят на ваши движения и
+                        передадут наслаждение вашему партнеру. Почувствуйте прикосновение вашего
+                        партнера по-новому. </p>
+                            </div>
+                    </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div class="big_separate">
+        <h1 class="larg white_text">
+            Take Control. Anytime. Anywhere.
+        </h1>
+    </div>
+    <div class="item_list inline">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 item">
+                    <div class="item_wrapper no_bord">
+                        <div class="item_thumb">
+                            <img src="assets/img/item_lush.jpg" alt="lush" class="img-center img-responsive" />
+                        </div>
+                        <div class="item_title">
+                            <h3>
+                                Lush
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 item">
+                    <div class="item_wrapper no_bord">
+                        <div class="item_thumb">
+                            <img src="assets/img/item_nora.jpg" alt="nora" class="img-center img-responsive" />
+                            <!--                        <div class="hidden_block overlay">-->
+                            <!--                        </div>-->
+                        </div>
+                        <div class="item_title">
+                            <h3>
+                                Nora
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 item">
+                    <div class="item_wrapper no_bord">
+                        <div class="item_thumb">
+                            <img src="assets/img/item_max.jpg" alt="max" class="img-center img-responsive" />
+                        </div>
+                        <div class="item_title">
+                            <h3>
+                                Max
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="button center mar_top_20 transparent">
+                       Подробнее
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="button center mar_top_20 transparent">
+                        Подробнее
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="button center mar_top_20 transparent">
+                        Подробнее
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </main>
